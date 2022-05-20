eef4f0caeafda7ce3805657c21c0d3794c3b08fd
cj-prince <davids.chijioke@gmail.com>
2022-05-20 14:26:13 +0100

    room model,admin setup and connection

--- base/views.py
+++ base/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': 'lets learn django'},
-    {'id': 2, 'name': 'lets learn django templates'},
-    {'id': 3, 'name': 'django frontend'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'lets learn django'},
+#     {'id': 2, 'name': 'lets learn django templates'},
+#     {'id': 3, 'name': 'django frontend'}
+# ]
 def home(request):
     return render(request, 'base/home.html', {'rooms':rooms})
 
