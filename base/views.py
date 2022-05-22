bf072edd44cdcd182bf35164d92944b2f2e9516c
cj-prince <davids.chijioke@gmail.com>
2022-05-22 20:29:56 +0100

    view and model update

--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 # rooms = [
@@ -9,12 +10,10 @@ from django.http import HttpResponse
 #     {'id': 3, 'name': 'django frontend'}
 # ]
 def home(request):
+    rooms = Room.objects.all()
     return render(request, 'base/home.html', {'rooms':rooms})
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] ==int(pk):
-            room=i
+    room = Room.objects.get(id=pk)
     context={'room':room}
     return render(request, 'base/room.html',context)
