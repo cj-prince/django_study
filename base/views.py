2990ff836057d1def7c1775450859f8ebdfcbdfb
cj-prince <davids.chijioke@gmail.com>
2022-05-23 20:56:11 +0100

    registration page added

--- base/views.py
+++ base/views.py
@@ -1,4 +1,3 @@
-from email import message
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic
@@ -8,6 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 # rooms = [
@@ -16,12 +16,12 @@ from django.contrib.auth.decorators import login_required
 #     {'id': 3, 'name': 'django frontend'}
 # ]
 def loginPage(request):
-    
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method =='POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user =User.objects.get(username=username)
@@ -35,12 +35,27 @@ def loginPage(request):
         else:
             messages.error(request, 'Username or Password does not exist') 
 
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    return render(request, 'base/login_register.html', {'form':form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ""
